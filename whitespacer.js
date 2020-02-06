4f9f2b7245c5a618b427c4ba24f0363185834588
lippielip <55237772+lippielip@users.noreply.github.com>
2020-02-06 14:21:36 +0100

    initial commit

---
+++ whitespacer.js
@@ -0,0 +1,14 @@
+let resultMargin = document.querySelectorAll('.g');
+let shitIcons = document.querySelectorAll('.xA33Gc, .GHDvE, .B6fmyf, .TbwUpd');
+let breaks = document.querySelectorAll('br');
+
+for(var i=0; i < resultMargin.length; i++){
+   resultMargin[i].style.marginBottom = "10px";
+}
+
+for(var i=0; i < shitIcons.length; i++){
+   shitIcons[i].remove();
+}
+for(var i=0; i < breaks.length; i++){
+   breaks[i].remove();
+}
